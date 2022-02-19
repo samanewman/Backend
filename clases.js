e9196531b31148cf15070f75b17edd6916f826fd
Samantha <newmansamantha00@gmail.com>
2022-02-19 10:44:10 -0300

    Clase 1

---
+++ clases.js
@@ -0,0 +1,20 @@
+class cliente{
+    constructor (nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this. apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+
+    }
+}
+
+const cliente1 =  new cliente (nombre, apellido, libros, mascotas){
+    nombre = "Pepe",
+    apellido = "Grillo",
+    libros = ["Duma Key", "Dark Tower", "Colorado kid"],
+    mascotas = ["Firu", "Satanás", "Fatiga"],
+
+};
+
+
+
