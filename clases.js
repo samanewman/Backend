48a7ff9508c91bca54cc920aa4ebbba9c736cb2e
Samantha <newmansamantha00@gmail.com>
2022-02-19 22:46:54 -0300

    Clase 1

--- clases.js
+++ clases.js
@@ -6,7 +6,7 @@ class cliente{
         this.mascotas = mascotas;
 
     }
-}
+};
 
 const cliente1 =  new cliente (nombre, apellido, libros, mascotas){
     nombre = "Pepe",
@@ -17,4 +17,14 @@ const cliente1 =  new cliente (nombre, apellido, libros, mascotas){
 };
 
 
+
+getFullName(){
+    console.log("Nombre y apellido:", this.cliente.nombre, this.cliente.apellido);
+
+};
+
+addMascota(nombremascota){
+    (this.cliente.mascotas).push(nombremascota);
+
+};
 
