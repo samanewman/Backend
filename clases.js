a8188d261746ceb9a96e1aeae18eacd544718c41
Samantha <newmansamantha00@gmail.com>
2022-02-21 16:49:36 -0300

    clase 1

--- clases.js
+++ clases.js
@@ -13,6 +13,7 @@ class cliente{
     
     addMascota(nombremascota){
         (this.mascotas).push(nombremascota);
+        console.log(this.mascotas);
     }
     
     countMascotas(){
@@ -20,36 +21,36 @@ class cliente{
     }
     
     addBook(addnombre, addautor){
-        (this.libros).push(nombre = addnombre, autor = addautor);
+        (this.libros).push({nombre: addnombre, autor: addautor});
+        console.log(this.libros);
     }
     
     getBookNames(){
-        console.log(this.libros.nombre);
+       for(const n in this.libros){
+           console.log(this.libros[n].nombre);
+       };
+    
     }
     
 
-}
+};
 
 
-
-
-
-
-const cliente1 =  new cliente (nombre, apellido, libros, mascotas){
+const cliente1 =  new cliente (
     nombre = "Pepe",
     apellido = "Grillo",
     libros = [
                 {nombre: "Duma Key", autor: "Stephen KIng"},
                 {nombre: "Dark Tower", autor: "Stephen King"},
-                {nombre: "La mujer ducha", autor: "Juan Sasturain"}
+                {nombre: "La mujer ducha", autor: "Juan Sasturain"},
             ],
-    mascotas = ["Firu", "Satanás", "Fatiga"]
+    mascotas = ["Firu", "Satanás", "Fatiga"],
+)
 
-}
 cliente1.getFullName();
 cliente1.addMascota("Sultán");
 cliente1.countMascotas();
-cliente1.addBook("Crónicas marcias", "Ray Bradbury");
+cliente1.addBook("Crónicas marcianas", "Ray Bradbury");
 cliente1.getBookNames();
 
 
